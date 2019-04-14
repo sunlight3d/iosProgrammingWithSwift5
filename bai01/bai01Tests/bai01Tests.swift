364e9b0389bec648fe876b5fdb44a6b653573f92
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-04-14 11:36:44 +0700

    update

---
+++ bai01/bai01Tests/bai01Tests.swift
@@ -0,0 +1,34 @@
+//
+//  bai01Tests.swift
+//  bai01Tests
+//
+//  Created by Nguyen Duc Hoang on 4/14/19.
+//  Copyright © 2019 Nguyen Duc Hoang. All rights reserved.
+//
+
+import XCTest
+@testable import bai01
+
+class bai01Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
